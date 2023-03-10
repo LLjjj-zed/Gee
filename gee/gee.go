53d0994adec862a781374e1cc5a42e58f9e1c343
“132982317” <2312680664@qq.com>
2023-03-10 13:40:51 +0800

    version 1.1.0

--- gee/gee.go
+++ gee/gee.go
@@ -1,25 +1,22 @@
 package gee
 
 import (
-	"fmt"
 	"net/http"
 	"strings"
 	"sync"
 )
 
-//实现了路由映射表，提供了用户注册静态路由的方法，包装了启动服务的函数
-
 // HandlerFunc defines the request handler used by gee
-type HandleFunc func(http.ResponseWriter, *http.Request)
+type HandlerFunc func(*Context)
 
 // Engine implement the interface of ServeHTTP
 type Engine struct {
-	router map[string]HandleFunc
+	router *router
 }
 
 // New is the constructor of gee.Engine
 func New() *Engine {
-	return &Engine{router: make(map[string]HandleFunc)}
+	return &Engine{router: NewRouter()}
 }
 
 var BufferPool = sync.Pool{
@@ -41,18 +38,17 @@ func Concate(s ...string) string {
 	return buf.String()
 }
 
-func (engine *Engine) addRoute(method string, parttern string, handler HandleFunc) {
-	key := Concate(method, "-", parttern)
-	engine.router[key] = handler
+func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
+	engine.router.addRoute(method, pattern, handler)
 }
 
 // GET defines the method to add GET request
-func (engine *Engine) GET(pattern string, handler HandleFunc) {
+func (engine *Engine) GET(pattern string, handler HandlerFunc) {
 	engine.addRoute("GET", pattern, handler)
 }
 
 // POST defines the method to add POST request
-func (engine *Engine) POST(pattern string, handler HandleFunc) {
+func (engine *Engine) POST(pattern string, handler HandlerFunc) {
 	engine.addRoute("POST", pattern, handler)
 }
 
@@ -62,10 +58,6 @@ func (engine *Engine) Run(addr string) (err error) {
 }
 
 func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	key := Concate(req.Method, "-", req.URL.Path)
-	if handler, ok := engine.router[key]; ok {
-		handler(w, req)
-	} else {
-		fmt.Fprintf(w, "404 NOT FOUND: %s\\n", req.URL)
-	}
+	c := newContext(w, req)
+	engine.router.handle(c)
 }
